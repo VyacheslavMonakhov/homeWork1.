78cf3337889b7dafc60c9b87029a2579b0bd5d7c
unknown <v.a.monahov@yandex.ru>
2020-08-20 18:22:46 +0300

    Initial commit

---
+++ src/Task_3.java
@@ -0,0 +1,9 @@
+public class Task3 {
+
+
+    public static void main(String[] args) {
+        int calculator(int a, int b, int c, int d); {
+            return a * (b + (c / d));
+        }
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    byte a=3;
+    short b=979;
+    int c=42_000;
+    long d=2_000_000_000_000_000L;
+    float e = 7.1f;
+    double f = 7.2;
+    char g = 'm';
+    boolean h=true;
+    String i="Hello variables!";
+
+}
