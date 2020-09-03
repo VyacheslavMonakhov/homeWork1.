4d093990bc9985aa79e5591cb0da400b6b152db2
Vyacheslav Monakhov <v.a.monahov@yandex.ru>
2020-09-03 13:32:20 +0300

    homework 1 (Lesson3)

--- src/Lesson3/Task1.java
+++ src/Lesson3/Task1.java
@@ -1,26 +1,48 @@
 package Lesson3;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Task1 {
 
+    private static final int ATTEMPT_NUMBER = 3;
+    private static final int MAX_VALUE = 9;
+
     public static void main(String[] args) {
+        Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ваша задача угадать число. У Вас есть 3 попытки");
-        int range = 9;
-        int number = (int) (Math.random()* range);
+
         while (true) {
-            System.out.println("Угадайте число от 0 до " + range);
-            int input_number = scanner.nextInt();
-            if (input_number == number) {
-                System.out.println("Вы угадали.");
+            playGame(random, scanner);
+
+            System.out.println("Повторить игру ещё раз? 1 - да / 0 - нет");
+
+            int result = scanner.nextInt();
+            if (result == 0) {
                 break;
-            } else if (input_number > number) {
+            }
+        }
+    }
+
+    private static void playGame(Random random, Scanner scanner) {
+        int targetValue = random.nextInt(MAX_VALUE + 1);
+        System.out.println("Ваша задача угадать число от 0 до 9. У Вас есть 3 попытки");
+
+        boolean isWin = false;
+        for (int attempt = 0; attempt < ATTEMPT_NUMBER; attempt++) {
+            System.out.printf("Попытка номер %d.%n", attempt + 1);
+            int input_number = scanner.nextInt();
+            if (input_number == targetValue) {
+                System.out.println("Вы угадали.");
+                isWin = true;
+                break;
+            } else if (input_number > targetValue) {
                 System.out.println("Загаданное число меньше");
             } else {
                 System.out.println("Загаданное число больше");
             }
         }
-        scanner.close();
+        String messageResult = isWin ? "Вы победитель!" : "Вы проиграли";
+        System.out.println(messageResult);
     }
 }
